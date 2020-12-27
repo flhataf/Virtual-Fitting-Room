24ba84d3b04cf9ec6ac43b1baf2f0f2c90c86f31
Sobii <moradiyan.sobhan@gmail.com>
2020-12-27 17:55:21 +0330

    The implementation of pose estimation! - 12/27/2020 - 5:53PM

---
+++ venv/VFR/flask_app.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template, Response
+from imutils.video import VideoStream
+from skeletonDetector import skeleton
+import cv2
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index2.html')
+
+def gen():
+
+    vs = VideoStream(src=0).start()
+
+    while cv2.waitKey(1) < 0:
+
+        frame = vs.read()
+
+        if frame is None:
+            continue
+
+        processed_fram = skeleton(frame)
+
+        (flag, encodedImage) = cv2.imencode(".jpg", processed_fram)
+
+        if not flag:
+            continue
+
+        yield (b'--frame\r\n' b'Content-Type: image/jpeg\r\n\r\n' +
+               bytearray(encodedImage) + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+
+    return Response(gen(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host="127.0.0.1", port=8086)
---
+++ venv/VFR/skeletonDetector.py
@@ -0,0 +1,77 @@
+import cv2
+import time
+import numpy as np
+
+# COCO Dataset -----------------------------
+protoFile = "pose_deploy_linevec.prototxt"
+weightsFile = "pose_iter_440000.caffemodel"
+nPoints = 18
+POSE_PAIRS = [[1, 0], [1, 2], [1, 5], [2, 3], [3, 4], [5, 6], [6, 7], [1, 8], [8, 9], [9, 10], [1, 11], [11, 12],
+              [12, 13], [0, 14], [0, 15], [14, 16], [15, 17]]
+
+#
+inWidth = 368
+inHeight = 368
+
+# inWidth = 168
+# inHeight = 168
+
+threshold = 0.3
+
+net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+
+def process(frame):
+    global net
+
+    t = time.time()
+
+    frameWidth = frame.shape[1]
+    frameHeight = frame.shape[0]
+
+    inpBlob = cv2.dnn.blobFromImage(frame, 1.0 / 255, (inWidth, inHeight), (0, 0, 0), swapRB=False, crop=False)
+    #TODO: blob google it
+    net.setInput(inpBlob)
+    output = net.forward()
+
+    H = output.shape[2]
+    W = output.shape[3]
+
+    # Empty list to store the detected keypoints
+    points = []
+
+    for i in range(nPoints):
+
+        # confidence map of corresponding body's part.
+        probMap = output[0, i, :, :]
+
+        # Find global maxima of the probMap.
+        minVal, prob, minLoc, point = cv2.minMaxLoc(probMap)
+
+        # Scale the point to fit on the original image
+        x = (frameWidth * point[0]) / W
+        y = (frameHeight * point[1]) / H
+
+        if prob > threshold:
+            # Add the point to the list if the probability is greater than the threshold
+            points.append((int(x), int(y)))
+        else:
+            points.append(None)
+
+    # Draw Skeleton
+    for pair in POSE_PAIRS:
+        partA = pair[0]
+        partB = pair[1]
+
+        if points[partA] and points[partB]:
+            cv2.line(frame, points[partA], points[partB], (0, 255, 255), 3, lineType=cv2.LINE_AA)
+            cv2.circle(frame, points[partA], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+            cv2.circle(frame, points[partB], 8, (0, 0, 255), thickness=-1, lineType=cv2.FILLED)
+
+    cv2.putText(frame, "time taken = {:.2f} sec".format(time.time() - t), (50, 50), cv2.FONT_HERSHEY_COMPLEX, .8,
+                (255, 50, 0), 2, lineType=cv2.LINE_AA)
+
+    return frame
+
+
+def skeleton(frame):
+    return process(frame)
